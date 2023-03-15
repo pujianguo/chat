f670c0345a088e1a2338d32d7788e3016711d615
pujianguo <18919015125@163.com>
2023-03-16 00:49:20 +0800

    update config

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@
 export default {
   env: import.meta.env.MODE,
   apiEnv: import.meta.env.MODE === 'production' ? 'prod' : 'test',
-  apiUrl: 'http://api.ckeyer.com',
+  apiUrl: 'https://api.ckeyer.com',
   urlPrefix: '/apis/p/',
   // apiUrl: import.meta.env.VITE_API_URL,
 }
